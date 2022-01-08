e6c1f31cff73270971383df69ce711b037f59e3c
Hai Nguyen <haind.vng@gmail.com>
2022-01-08 19:49:41 +0700

    init project

---
+++ src/main/java/com/bootiful/admin/BootifulAdminApplication.java
@@ -0,0 +1,13 @@
+package com.bootiful.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulAdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootifulAdminApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bootiful/admin/BootifulAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootiful.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulAdminApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
