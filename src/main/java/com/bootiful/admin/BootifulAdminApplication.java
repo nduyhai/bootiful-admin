138840d5de0958b86f9d11fa8fb3be9dac0a2955
Hai Nguyen <haind.vng@gmail.com>
2022-01-08 20:23:16 +0700

    add admin client

--- src/main/java/com/bootiful/admin/BootifulAdminApplication.java
+++ src/main/java/com/bootiful/admin/BootifulAdminApplication.java
@@ -2,7 +2,10 @@ package com.bootiful.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
+
+@EnableAdminServer
 @SpringBootApplication
 public class BootifulAdminApplication {
 
